9daa7c609bd023c3acf89e548f1873d2029ec290
Gina Guerrero <gina.guerrero@bigbear.io>
2018-12-10 10:33:18 -0800

    Corrections to day 2. Added day 3 input.

--- 2018/day2.py
+++ 2018/day2.py
@@ -1,10 +1,12 @@
 INPUT_DATA = 'day2_input.txt'
 sample_data = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']
 
+
 def read_input_data(infile=INPUT_DATA): 
     with open(infile, 'r') as f: 
         data = [x.strip() for x in f.readlines()]
     return data 
+
 
 def get_counts(in_data, checksum_only=True):
     counts = {2: 0, 3: 0}
@@ -16,6 +18,7 @@ def get_counts(in_data, checksum_only=True):
         return counts[2] * counts[3]
     return counts
 
+
 def find_similar_ids(in_data, common_only=True):
     def is_diff_by_one(str1, str2):
         results = []
@@ -23,13 +26,14 @@ def find_similar_ids(in_data, common_only=True):
             results.append(str1[i] == str2[i])
         return results.count(False) == 1, results
 
-    for s1 in in_data[:-1]:
-        for s2 in in_data[1:]:
+    for indx, s1 in enumerate(in_data[:-1], 1):
+        for s2 in in_data[indx:]:
             is_diff, diff_res = is_diff_by_one(s1, s2)
             if is_diff and common_only:
                 return [s1[i] for i in range(len(s1)) if diff_res[i] is True]
             elif is_diff and not common_only:
                 return s1, s2
+
 
 in_data = read_input_data()
 
