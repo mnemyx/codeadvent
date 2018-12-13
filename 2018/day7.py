094b0e29935f3b9ec0f071d6f76a21cd9813b88e
Gina Guerrero <gina.guerrero@bigbear.io>
2018-12-13 15:02:37 -0800

    Day 7 (actually)

--- 2018/day7.py
+++ 2018/day7.py
@@ -1,7 +1,11 @@
 import re
-from collections import namedtuple, defaultdict
+from string import ascii_uppercase
+from collections import defaultdict
 
-INPUT_DATA = 'day7_input.txt'
+from commonlib import determine_day, read_input_data
+
+
+DAY = determine_day(__file__)
 SAMPLE_DATA = [
     'Step C must be finished before step A can begin.',
     'Step C must be finished before step F can begin.',
@@ -13,88 +17,119 @@ SAMPLE_DATA = [
 ]
 
 
-Coords = namedtuple('Coords', ['x', 'y'])
-
-
-def read_input_data(infile=INPUT_DATA): 
-    with open(infile, 'r') as f: 
-        data = [x.strip() for x in f.readlines()]
-    return data 
-
-
-def convert_to_coords(in_data, delim=","):
-    coordinates = []
-    for line in in_data:
-        points = [int(p.strip()) for p in line.split(delim)]
-        coordinates.append(Coords(x=points[0], y=points[1]))
-    return coordinates
-
-
-def calc_manhattan_dist(c1, c2):
-    return abs(c1.x - c2.x) + abs(c1.y - c2.y)
-
-
-def find_min_max(coords):
-    p_x = [c.x for c in coords]
-    p_y = [c.y for c in coords]
-    return min(p_x), max(p_x), min(p_y), max(p_y)
-
-
-def map_and_calc_areas(coords):
-    # Find min/max x, y coordinates
-    min_x, max_x, min_y, max_y = find_min_max(coords)
-    cached = dict()
-    mapped = defaultdict(list)
-    infinites = set()
-    for coord in coords:
-        if coord.x in (min_x, max_x) or coord.y in (min_y, max_y):
-            infinites.add(coord)
-        for x in range(min_x, max_x + 1):
-            for y in range(min_y, max_y + 1):
-                if (x, y) in cached:
-                    tmp = cached[(x, y)]
-                else:
-                    tmp = Coords(x=x, y=y)
-                man_dist = calc_manhattan_dist(tmp, coord)
-                mapped[tmp].append((coord, man_dist))
-
-    areas = defaultdict(int)
-    for m, data in mapped.items():
-        sorted_data = sorted(data, key=lambda x: x[1])
-        # Coordinates with infinite areas aren't always
-        # the ones with min/max x, y coordinates. They're
-        # also any coordinates whose area range manage to 
-        # reach said min/max with the shortest distance.
-        if m.x in (min_x, max_x) or m.y in (min_y, max_y):
-            infinites.add(sorted_data[0][0])
-        if [d for c, d in sorted_data].count(sorted_data[0][1]) == 1:
-            areas[sorted_data[0][0]] += 1
-    return mapped, areas, infinites
-
-
-def find_largest_area(areas, infinites=(), exclude_infinites=True):
-    areas_sorted = []
-    for c, a in sorted(areas.items(), key=lambda kv: kv[1], reverse=True): 
-        if exclude_infinites and c in infinites:
+def parse_dependencies(in_data):
+    steps = set()
+    dependencies = defaultdict(list)
+    regex = re.compile(r"Step\s(?P<catalyst>[A-Z]).+step\s(?P<step>[A-Z]).+")
+    for line_no, line in enumerate(in_data):
+        res = regex.match(line)
+        if not res:
+            print("Error parsing line #{0}: {1}".format(line_no, line))
             continue
-        areas_sorted.append((c, a))
-    return areas_sorted[0]
+        steps.add(res['catalyst'])
+        dependencies[res['step']].append(res['catalyst'])
+    for step in steps:
+        if step not in dependencies:
+            dependencies[step] = []
+    return dict([(s, sorted(c)) for (s, c) in dependencies.items()])
 
 
-def find_most_nearby_coords(mapped, max_dist_sum=10000):
-    acceptable = list()
-    for m, data in mapped.items():
-        if sum([d for c, d in data]) < max_dist_sum:
-            acceptable.append(m)
-    return acceptable
+def sort_by_catalysts(dependencies):
+    by_catalysts = []
+    # Sort dependencies by lenth of value then their key
+    for s, a in sorted(dependencies.items(), key=lambda kv: (len(kv[1]), kv[0])): 
+        by_catalysts.append((s, a))
+    return by_catalysts
 
 
-in_data = read_input_data()
-coords = convert_to_coords(in_data)
-mapped, areas, infinites = map_and_calc_areas(coords)
+def queue_ready_steps(by_catalysts, completed):
+    ready = []
+    for step, catalysts in by_catalysts:
+        if step in completed:
+            continue
+        if (len(catalysts) == 0 and step not in completed) or \
+            all((s in completed) for s in catalysts):
+            ready.append(step)
+    return ready
 
-p1 = find_largest_area(areas, infinites)
-print("Part 1:", p1[0], p1[1])
 
-p2 = find_most_nearby_coords(mapped)
-print("Part 2:", len(p2))
+def determine_step_order(dependencies):
+    completed = []
+    by_catalysts = sort_by_catalysts(dependencies)
+
+    queue = set()
+    while len(completed) != len(dependencies):
+        queue.update(queue_ready_steps(by_catalysts, completed))
+        next_step = sorted(queue).pop(0)
+        completed.append(next_step)
+        queue.remove(next_step)
+    return completed
+
+
+def simulate_worker_queue(dependencies, workers=5, add_sec=60):
+    by_catalysts = sort_by_catalysts(dependencies)
+    task_queue = set()
+    completed = []
+    assigned = []
+    # Worker 0 == "Me"
+    worker_queue = dict((x, None) for x in range(0, workers))
+    # Map task to its expected taks time
+    task_time = dict((ta, ti) for (ti, ta) in enumerate(ascii_uppercase, start=add_sec+1))
+
+    def _ready_workers():
+        return [x for x in worker_queue if worker_queue[x] is None]
+
+    def _assign_task(task, wid):
+        worker_queue[wid] = [task, task_time[task]]
+        return task
+
+    def _tick_time_down():
+        tasks_done = []
+        for wid, task in worker_queue.items():
+            if task is None:
+                continue
+            task[1] -= 1
+            if task[1] == 0:
+                tasks_done.append(task[0])
+                worker_queue[wid] = None
+        return tasks_done
+    
+    time_spent = 0
+    while len(completed) != len(dependencies):
+        # Check for ready tasks, aka their order based on completed tasks
+        task_queue.update(queue_ready_steps(by_catalysts, completed))
+        # But avoid re-assigned currently assigned tasks
+        task_queue = task_queue.difference(set(assigned))
+        # If no available workers or takss, then there's no point
+        # in doing the extra work to figure tasks + assigning
+        if _ready_workers() and task_queue:
+            for wid in _ready_workers():
+                if task_queue:
+                    # Assign the next task and remove it from queue
+                    task = _assign_task(sorted(task_queue)[0], wid)
+                    assigned.append(task)
+                    task_queue.remove(task)
+        # Tick down the time
+        completed.extend(_tick_time_down())
+        time_spent += 1
+    return time_spent, completed
+
+
+def exec_day(sample=False):
+    if sample:
+        in_data = SAMPLE_DATA
+        workers = 2
+        add_sec = 0
+    else:
+        in_data = read_input_data(DAY)
+        workers = 5
+        add_sec = 60
+    
+    dependencies = parse_dependencies(in_data)
+
+    p1 = determine_step_order(dependencies)
+    print("Part 1:", ''.join(p1))
+
+    p2 = simulate_worker_queue(dependencies, workers=workers, add_sec=add_sec)
+    print("Part 2:", p2[0], ''.join(p2[1]))
+
