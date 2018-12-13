333b49a38e376b468922727b941445b099c00c71
Gina Guerrero <gina.guerrero@bigbear.io>
2018-12-13 12:41:20 -0800

    README

---
+++ 2018/day7.py
@@ -0,0 +1,100 @@
+import re
+from collections import namedtuple, defaultdict
+
+INPUT_DATA = 'day7_input.txt'
+SAMPLE_DATA = [
+    'Step C must be finished before step A can begin.',
+    'Step C must be finished before step F can begin.',
+    'Step A must be finished before step B can begin.',
+    'Step A must be finished before step D can begin.',
+    'Step B must be finished before step E can begin.',
+    'Step D must be finished before step E can begin.',
+    'Step F must be finished before step E can begin.',
+]
+
+
+Coords = namedtuple('Coords', ['x', 'y'])
+
+
+def read_input_data(infile=INPUT_DATA): 
+    with open(infile, 'r') as f: 
+        data = [x.strip() for x in f.readlines()]
+    return data 
+
+
+def convert_to_coords(in_data, delim=","):
+    coordinates = []
+    for line in in_data:
+        points = [int(p.strip()) for p in line.split(delim)]
+        coordinates.append(Coords(x=points[0], y=points[1]))
+    return coordinates
+
+
+def calc_manhattan_dist(c1, c2):
+    return abs(c1.x - c2.x) + abs(c1.y - c2.y)
+
+
+def find_min_max(coords):
+    p_x = [c.x for c in coords]
+    p_y = [c.y for c in coords]
+    return min(p_x), max(p_x), min(p_y), max(p_y)
+
+
+def map_and_calc_areas(coords):
+    # Find min/max x, y coordinates
+    min_x, max_x, min_y, max_y = find_min_max(coords)
+    cached = dict()
+    mapped = defaultdict(list)
+    infinites = set()
+    for coord in coords:
+        if coord.x in (min_x, max_x) or coord.y in (min_y, max_y):
+            infinites.add(coord)
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                if (x, y) in cached:
+                    tmp = cached[(x, y)]
+                else:
+                    tmp = Coords(x=x, y=y)
+                man_dist = calc_manhattan_dist(tmp, coord)
+                mapped[tmp].append((coord, man_dist))
+
+    areas = defaultdict(int)
+    for m, data in mapped.items():
+        sorted_data = sorted(data, key=lambda x: x[1])
+        # Coordinates with infinite areas aren't always
+        # the ones with min/max x, y coordinates. They're
+        # also any coordinates whose area range manage to 
+        # reach said min/max with the shortest distance.
+        if m.x in (min_x, max_x) or m.y in (min_y, max_y):
+            infinites.add(sorted_data[0][0])
+        if [d for c, d in sorted_data].count(sorted_data[0][1]) == 1:
+            areas[sorted_data[0][0]] += 1
+    return mapped, areas, infinites
+
+
+def find_largest_area(areas, infinites=(), exclude_infinites=True):
+    areas_sorted = []
+    for c, a in sorted(areas.items(), key=lambda kv: kv[1], reverse=True): 
+        if exclude_infinites and c in infinites:
+            continue
+        areas_sorted.append((c, a))
+    return areas_sorted[0]
+
+
+def find_most_nearby_coords(mapped, max_dist_sum=10000):
+    acceptable = list()
+    for m, data in mapped.items():
+        if sum([d for c, d in data]) < max_dist_sum:
+            acceptable.append(m)
+    return acceptable
+
+
+in_data = read_input_data()
+coords = convert_to_coords(in_data)
+mapped, areas, infinites = map_and_calc_areas(coords)
+
+p1 = find_largest_area(areas, infinites)
+print("Part 1:", p1[0], p1[1])
+
+p2 = find_most_nearby_coords(mapped)
+print("Part 2:", len(p2))
--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Advent of Code
 
-https://adventofcode.com/
+### https://adventofcode.com/
 
-Python solutions, because Python.
+- Python solutions, because Python.
+- Since this is for fun, I'm _not_ going for:
+  - Completion Speed
+  - Execution Speed
+  - Minimalistic Code
+- I just wanted to solve the puzzles.
+
+
+### Examples
+##### Run with sample data, usually given in the problem or slightly modified
+```python3
+python 2018/aoc.py -s 6
+```
+
+##### Run with my input data (may be long running)
+```python3
+python 2018/aoc.py 6
+```
+
+##### Run multiple days
+```python3
+python 2018/aoc.py 3 4 6
+```
